06a72a84e7fe47840b52eda4806c04e88095bfae
Felicia <feliciaaa.xiao@gmail.com>
2025-04-03 00:44:06 -0700

    Added a front-end skeleton.

---
+++ chatbot-ui/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { AuthProvider, AuthContext } from "./AuthContext";
+import Login from "./Login";
+import Chat from "./Chat";
+
+const PrivateRoute = ({ children }) => {
+  const { token } = React.useContext(AuthContext);
+  return token ? children : <Navigate to="/login" />;
+};
+
+function App() {
+  return (
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route 
+            path="/chat" 
+            element={
+              <PrivateRoute>
+                <Chat />
+              </PrivateRoute>
+            } 
+          />
+          <Route path="/" element={<Navigate to="/chat" />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
+}
+
+export default App;
